4340b6aa8d4c73a4f392851897fe46e147b63133
tostimontes <147332922+tostimontes@users.noreply.github.com>
2024-03-05 10:43:57 +0100

    feat: toggle image info w/ rotation

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,12 @@
-export default function Card({ source, onClick }) {
-  return <img src={source} alt="" onClick={onClick} />;
+export default function Card({ source, onClick, isInfoMode }) {
+  return (
+    <div className="card" onClick={onClick}>
+      <div className={`card-inner ${isInfoMode ? 'hover' : ''}`}>
+        <div className="card-front">
+          <img src={source} alt="" />
+        </div>
+        <div className="card-back">{source}</div>
+      </div>
+    </div>
+  );
 }
